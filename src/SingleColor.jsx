13d166559cd30ddafcc9c41262e098696222960b
Ansoumane Soumaoro <ansoumanesoumaoro07@gmail.com>
2024-01-16 02:56:01 +0100

    first commit

---
+++ src/SingleColor.jsx
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+
+function SingleColor({ color, index }) {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        toast.error("Failed copied to clipboard");
+      }
+    } else {
+      toast.error("fail copied to clipboard");
+    }
+  };
+
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
+}
+
+export default SingleColor;
